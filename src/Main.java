faf92a0114f799ec2d57db433233e9b5b44d8af0
OLIVER MURRAY <123483518+olivermurray56@users.noreply.github.com>
2023-01-24 15:40:04 +0000

    added comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.*;
 public class Main{
     public static void main(String[] args)
     {
+    System.out.println("made a comment in replit");
         JFrame myFrame = new JFrame();
         myFrame.setSize(500, 500);
 
