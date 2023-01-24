cefea5751ad9466bf1c9ed93aad33160cc2a987f
oliver.murray24 <oliver16murray@gmail.com>
2023-01-24 09:43:04 -0600

    made comment in intelij

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main{
     public static void main(String[] args)
     {
     System.out.println("made a comment in replit");
+    System.out.println("comment from intelij");
         JFrame myFrame = new JFrame();
         myFrame.setSize(500, 500);
 
